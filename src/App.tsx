1e5382db7dafbd77490a17d237142d07e0ec6fe7
KW Chae <wolseo1@gmail.com>
2024-03-13 04:23:48 +0900

    feat: Implement basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './Home';
+import GenerateCard from './api/GenerateCard';
 
 function App() {
 
   return (
     <>
-      AC-counter
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/api/generate_card" element={<GenerateCard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/api/GenerateCard.tsx
@@ -0,0 +1,78 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Unstable_Grid2';
+import Sites from "./Sites.json";
+import './GenerateCard.css';
+
+interface info {
+    siteName: string | null,
+    handle: string | null,
+    solved: number | null,
+};
+
+const GenerateCard = () => {
+    const [params, _] = useSearchParams();
+    const [siteInfo, setSiteInfo] = useState<info[]>([]);
+
+    // const getBOJInfo = async () => {
+    //     const url = `https://solved.ac/api/v3/user/show?handle=${bojHandle}`;
+    //     const response = await fetch(`https://api-py.vercel.app/?r=${url}`, {
+    //         method: "GET"
+    //     });
+    //     const data = await response.json();
+    //     console.log(data);
+    // }
+
+    useEffect(() => {
+        let newInfo: info[] = [];
+        Object.values(Sites).forEach((site) => {
+            const siteName = site.name.toLowerCase();
+            if(params.has(siteName)) {
+                // console.log(site.name, params.get(site.name));
+                newInfo.push({
+                    siteName: site.name,
+                    handle: params.get(siteName),
+                    solved: 0
+                });
+            }
+        });
+        setSiteInfo(newInfo);
+    }, []);
+
+    return (
+        <>
+            <Box
+                height={170}
+                width={350}
+                display="flex"
+                alignItems="center"
+                justifyContent={"space-between"}
+                fontFamily="Pretendard"
+                sx={{ border: '2px solid grey' }}
+            >
+                <div className="card-sites-wrapper">
+                    {
+                        siteInfo.map((info) => {
+                            return (
+                                <div className="card-site-wrapper">
+                                    <div style={{width: "100px  "}}>{info.siteName}</div>
+                                    <div style={{width: "50px"}}>{info.handle}</div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <div className="card-count-wrapper">
+                    Total solved
+                    <div className="card-count">
+                        {0}
+                    </div>
+                </div>
+            </Box>
+        </>
+    );
+}
+
+export default GenerateCard;
